757998ea7a6205287b1e6be91c77c7e62382de51
Aaron Bernstein <aaron@stitchmedia.ca>
2017-10-17 13:39:44 -0400

    in theory, this runs 50% as many jobs in parallel

--- main.js
+++ main.js
@@ -3,12 +3,20 @@ var ffmpeg = require('fluent-ffmpeg');
 var through2 = require('through2');
 var concat = require('concat-stream');
 
+// executes an array of promises in sequence, and returns an array of all their outputs
+// mostly copied from https://hackernoon.com/functional-javascript-resolving-promises-sequentially-7aac18c4431e
+const promiseSerial = promises =>
+    promises.reduce((reduced, promise) =>
+        promise.then(result => promise.then(Array.prototype.concat.bind(result))),
+        Promise.resolve([]));
+
 module.exports = function(content) {
     const options = loaderUtils.getOptions(this);
 
     const context = options.context || this.options.context;
 
     var outputPattern = options.name || "[name]-[hash].[ext]";
+    this.cacheable();
     var callback = this.async();
 
 // stream to buffer
@@ -28,8 +36,24 @@ module.exports = function(content) {
             input.end(content);
             input.isStream = true; // hack, but seems to work
             ffmpeg()
+            //   .on('start', function(commandLine) {
+            //     console.log('Spawned Ffmpeg with command: ' + commandLine);
+            //   })
+            //   .on('codecData', function(data) {
+            //     console.log('Input is ' + data.audio + ' audio ' + 'with ' + data.video + ' video');
+            //   })
+            //   .on('progress', function(progress) {
+            //     console.log('Processing: ' + progress.percent + '% done');
+            //   })
+            //   .on('stderr', function(stderrLine) {
+            //     console.log('Stderr output: ' + stderrLine);
+            //   })
+            //   .on('end', function(stdout, stderr) {
+            //     console.log('Transcoding succeeded !');
+            //   })
               .on('error', function(err, stdout, stderr) {
-                    console.log('An error occurred: ' + err.message, err, stderr);
+                  emitError('Cannot process audio: ' + err.message);
+                  reject(err);
               })
               .input(input)
               .audioBitrate('128k')
@@ -41,7 +65,7 @@ module.exports = function(content) {
           });
     }
 
-    Promise.all([doFfmpeg('mp3'), doFfmpeg('ogg')]).then(outputs=>{
+    promiseSerial([doFfmpeg('mp3'), doFfmpeg('ogg')]).then(outputs=>{
         var finaloutput = "module.exports = [" + outputs.join(',') + "];";
         callback(null, finaloutput);
     });
